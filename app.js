a1e1030a0d8b4ac2178cec988b882326b5940e09
Tony Bogun <antonybogun@gmail.com>
2017-11-12 00:40:34 -0500

    Add dropdown integration

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ var client = platformClient.ApiClient.instance;
 var clientId = '1a469bb5-7bb7-4fa7-bf4e-6e90191223a8';
 redirectUri = 'https://localhost:3000/' // encodeURIComponent(window.location.href);
 
-
+function predict(){
+    authStuff();
+}
 
 let authStuff = async () => {
+
+    let it = document.getElementById("interactionType");
+    let interactionType = it.options[it.selectedIndex].value;
+
+    let m = document.getElementById("metric");
+    let metric = m.options[m.selectedIndex].value;
+
+    console.log(interactionType);
+    console.log(metric);
+
     let body = `
     {
         "interval": "2017-10-11T04:00:00.000Z/2017-11-12T05:00:00.000Z",
@@ -25,32 +37,36 @@ let authStuff = async () => {
     let dataObj = {};
 
     let mediaTypeData = data.results.filter(function(item){
-         return item.group.mediaType === 'email';
+         return item.group.mediaType === interactionType;
       })[0].data;
-
+    console.log(mediaTypeData);
     mediaTypeData.forEach(element => {
         let keyDate = element.interval.split('/')[0];
         console.log(element);
 
         let valueStats = element.metrics.filter(function(item){
-            return item.metric === 'tWait';
-        })[0].stats;
-        dataObj[keyDate] = valueStats;        
-        
+            return item.metric === metric;
+        });
+        if(valueStats.length > 0) {
+            valueStats = valueStats[0].stats;
+            dataObj[keyDate] = valueStats;
+        }        
     });
 
     json['data'] = dataObj;
-    json['predictDays'] = 7;
+    json['predictDays'] = 1;
     json['politeMode'] = 'please';
+
+    console.log(JSON.stringify(json));
     
-    let url = 'http://localhost:8000/predictPlease';
+    let url = 'https://ccforecast.herokuapp.com/predictPlease';
     
     fetch(url, {
         method: 'post',
         headers: {
           "Content-type": "application/json; charset=UTF-8"
         },
-        body: json
+        body: JSON.stringify(json)
       })
       .then(json)
       .then(function (data) {
@@ -59,16 +75,12 @@ let authStuff = async () => {
       .catch(function (error) {
         console.log('Request failed', error);
       });
-
-    
 }
-
-
 
 client.loginImplicitGrant(clientId, redirectUri)
     .then(function () {
         //alert('I am auth');
-        authStuff();
+        //authStuff();
     })
     .catch(function (response) {
         console.log(`${response.status} - ${response.error.message}`);
